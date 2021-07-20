c6a7b6b7936f9032445d0b929fea7ddd5ee21532
Jann Slettebakk <jkslettebakk@yahoo.no>
2021-07-20 17:15:18 +0200

    New HAN-OO solution

---
+++ HAN-OO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HAN_OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            foreach( var a in args)
+                Console.WriteLine("{0}",a);
+            
+        }
+    }
+}
