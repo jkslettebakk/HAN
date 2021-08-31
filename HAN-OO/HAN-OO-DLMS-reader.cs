c3d418ee9a9f4b61d764cf701fb8ba22433bbe33
Jann Slettebakk <jkslettebakk@yahoo.no>
2021-08-31 21:35:56 +0200

    Object handling restructured....

--- HAN-OO/HAN-OO-DLMS-reader.cs
+++ HAN-OO/HAN-OO-DLMS-reader.cs
@@ -3,15 +3,17 @@ namespace HANDLMS
 {
     public class OO_HAN_DLMS_Read
     {
-        public SerialPort serialPort = new SerialPort();
-        public OOUserConfigurationParameters.UserConfigurationParameters ucp = new OOUserConfigurationParameters.UserConfigurationParameters();
+        // public OOUserConfigurationParameters.UserConfigurationParameters ucp = new OOUserConfigurationParameters.UserConfigurationParameters();
+        public OOUserConfigurationParameters.UserConfigurationParameters ucp;
         public OOUserConfigurationParameters ucpPortData = new OOUserConfigurationParameters();
         public byte[] HANData = new byte[100];
 
-        public void OO_HAN_DLMS_Open()
+        public void OO_HAN_DLMS_Open( SerialPort serialPort )
         {
             Console.WriteLine("Opening port:{0}",ucp.HANOODeviceData.serialPortName);
             serialPort.Open();
+            Console.WriteLine("Port open:{0}",ucp.HANOODeviceData.serialPortName);
+            serialPort.Close();
         }
 
         public OO_HAN_DLMS_Read()
@@ -19,16 +21,17 @@ namespace HANDLMS
             Console.WriteLine("Initialising/preparing \"HANDLMS\" objects and port reading");
         }
 
-        public void OO_HAN_DLMS_ReadData( )
+        public void OO_HAN_DLMS_ReadData( SerialPort serialPort )
         {
-            serialPort = ucpPortData.getPortParameters();
+            serialPort = ucpPortData.getPortParameters( serialPort );
+            Console.WriteLine("Input port:{0}",serialPort.PortName);
             Console.WriteLine("Prepare reading HAN port:{0}",ucp.HANOODeviceData.serialPortName);
             serialPort.PortName = ucp.HANOODeviceData.serialPortName;
             serialPort.BaudRate = ucp.HANOODeviceData.serialPortBaud;
             serialPort.Parity = ucp.HANOODeviceData.serialPortParity;
             serialPort.DataBits = ucp.HANOODeviceData.serialPortDataBits;
             serialPort.StopBits = ucp.HANOODeviceData.serialPortStopBits;
-            OO_HAN_DLMS_Open();
+            OO_HAN_DLMS_Open( serialPort );
         }
 
         public class DLMSdataBlock
